4b105bacf099d10f288d128ac9f0fc70b0ffac6a
davidmgudeman <davidmgudeman@gmail.com>
2016-11-17 14:55:50 -0800

    Week04 Exercise01 completed - mogoose population

---
+++ rest-server-passport/README.md
@@ -0,0 +1,2 @@
+Week04 Coursera_ServerSideDevelopmentWithNodeJS_Week04
+Back end as a Service (Baas)
--- rest-server-passport/routes/users.js
+++ rest-server-passport/routes/users.js
@@ -18,10 +18,18 @@ router.post('/register', function(req, res) {
       req.body.password, function(err, user) {
         if (err) {
             return res.status(500).json({err: err});
-        }
+      }
+      if(req.body.firstname) {
+        user.firstname = req.body.firstname;
+      }
+      if(req.body.lastname) {
+        user.lastname = req.body.lastname;
+      }
+      user.save(function(err, user){
         passport.authenticate('local')(req, res, function () {
             return res.status(200).json({status: 'Registration Successful!'});
         });
+      });
     });
 });
 
