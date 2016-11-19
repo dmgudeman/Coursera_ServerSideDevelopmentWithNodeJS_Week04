4c709a88558ccad98697f355f730b84db7000f3c
davidmgudeman <davidmgudeman@gmail.com>
2016-11-18 17:03:28 -0800

    Week03 Exercise03 Oauth is working

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '640277842819895',
+        clientSecret: 'e441da773fa7d627a2898f194f3a24d7',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
