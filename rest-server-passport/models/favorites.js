13e5dc4e43fac604503613ed448769bf70fa46be
davidmgudeman <davidmgudeman@gmail.com>
2016-12-16 11:47:16 -0800

    Used the update method and it working much more elegantly

--- rest-server-passport/models/favorites.js
+++ rest-server-passport/models/favorites.js
@@ -1,34 +1,7 @@
 // grab the things we need
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-// require('mongoose-currency').loadType(mongoose);
-// var Currency = mongoose.Types.Currency;
 
-// var commentSchema = new Schema({
-//     rating:  {
-//         type: Number,
-//         min: 1,
-//         max: 5,
-//         required: true
-//     },
-//     comment:  {
-//         type: String,
-//         required: true
-//     },
-//     postedBy:  {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User'
-//     }
-// }, {
-//     timestamps: true
-// });
-
-// create a schema for image type
-//var ImageSchema = new Schema({
- // url : { type : String},
- // created : { type : Date, default : Date.now }
-//});
-//var Image = db.model('images', ImageSchema);
 
 // create a schema
 var favoriteSchema = new Schema({
--- rest-server-passport/routes/favoriteRouter.js
+++ rest-server-passport/routes/favoriteRouter.js
@@ -23,44 +23,34 @@ favoriteRouter.route('/')
 
     .post(function (req, res, next) {
 
-        Favorites.find({'postedBy': req.decoded._doc._id})
-            .exec(function (err, favorites) {
+        Favorites.update({'postedBy': req.decoded._doc._id}, {$push: {dishes:req.body._id}} ,function (err, favorite) {
                 if (err) throw err;
-                req.body.postedBy = req.decoded._doc._id;
+            res.json(favorite);
 
-                if (favorites.length) {
-                    var favoriteAlreadyExist = false;
-                    if (favorites[0].dishes.length) {
-                        for (var i = (favorites[0].dishes.length - 1); i >= 0; i--) {
-                            favoriteAlreadyExist = favorites[0].dishes[i] == req.body._id;
-                            if (favoriteAlreadyExist) break;
-                        }
-                    }
-                    if (!favoriteAlreadyExist) {
-                        favorites[0].dishes.push(req.body._id);
-                        favorites[0].save(function (err, favorite) {
-                            if (err) throw err;
-                            console.log('Um somethings up!');
-                            res.json(favorite);
-                        });
-                    } else {
-                        console.log('Setup!');
-                        res.json(favorites);
-                    }
 
-                } else {
-
-                    Favorites.create({postedBy: req.body.postedBy}, function (err, favorite) {
-                        if (err) throw err;
-                        favorite.dishes.push(req.body._id);
-                        favorite.save(function (err, favorite) {
-                            if (err) throw err;
-                            console.log('Something is up!');
-                            res.json(favorite);
-                        });
-                    })
-                }
-            });
+    //            if (favorite.dishes) {
+    //
+    //                    favorite.dishes.push(req.body._id);
+    //                    favorite.dishes.save(function (err, favorite) {
+    //                        if (err) throw err;
+    //                        console.log('pushed one!');
+    //                        res.json(favorite);
+    //                    });
+    //
+    //
+    //            } else {
+    //
+    //                Favorites.create({postedBy: req.body.postedBy}, function (err, favorite) {
+    //                    if (err) throw err;
+    //                    favorite.dishes.push(req.body._id);
+    //                    favorite.save(function (err, favorite) {
+    //                        if (err) throw err;
+    //                        console.log('created and pushed one');
+    //                        res.json(favorite);
+    //                    });
+    //                })
+    //            }
+           });
     })
 
 
